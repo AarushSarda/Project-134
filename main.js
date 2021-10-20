2fba23aa32e0aed4e35ce698ba3e3ce168eae5fc
Aarush Sarda !! <77834111+AarushSarda@users.noreply.github.com>
2021-10-20 16:17:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+alarm = "";
+status = "";
+objects = [];
+function preload() {
+    alarm = loadSound("alarm.mp3");
+}
+function setup() {
+    canvas = createCanvas(380 , 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+}
+function gotResults(error , results) {
+    if (error) {
+        console.log (error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+function draw() {
+    image(video,0,0,380,380);
+    if(status != ""){
+        r = random(255);
+        b = random(255);
+        g = random(255);
+        objectDetector.detect(video , gotResults);
+        for(i=0 ; i<objects.length ; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";document.getElementById("n0_of_objects").innerHTML = "Number of Objects Detected are : " + objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "  " + percent + " %" , objects[i].x + 15 , objects[i].y + 15); 
+            noFill();
+            stroke(r,g,b);
+            rect( objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+            if(objects[i].label == "person") {
+                document.getElementById("found").innerHTML = "Baby Found";
+                alarm.stop();
+                console.log("Stop");
+            }
+            else{
+                document.getElementById("found").innerHTML = "Baby Not Found";
+                alarm.play();
+                console.log("Play");
+            }
+        }
+        if(objects.length == 0) {
+            document.getElementById("found").innerHTML = "Baby Not Found";
+            alarm.play();
+            console.log("Play");
+        }
+    }
+}
